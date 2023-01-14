702839b98dce4ee458e2ba64e5a4362cebc27ad4
digitinok <75326757+digitinok@users.noreply.github.com>
2023-01-14 09:25:20 +0000

    create random paassword

--- script.js
+++ script.js
@@ -90,17 +90,21 @@ const upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  // Reading in the expected length of the password and checking the size and format
+  // declaring variables
   let passwordLength;
+
+  // Reading in the expected length of the password and checking the size and format
   do {
 	  passwordLength = parseInt(prompt("How many character shall you password have?\n Password length should be between 10 and 69 characters"));
+    console.log(passwordLength)
   } while (passwordLength < 10 || passwordLength > 69 || isNaN(passwordLength)); 
   
   // getting the expected characters in the password
+  alert("please choose at least one of the follwing four options:")
   const lower = confirm("Would you like to use lower case letters a-z in your password?");
-  const upper = confirm("Would you like to use upper case letters A-Zin your password?");
+  const upper = confirm("Would you like to use upper case letters A-Z in your password?");
   const numbers = confirm("Would you like to use numbers 0-9 in your password?");
-  const special = confirm("Would you like to use special charactersin your password?");
+  const special = confirm("Would you like to use special characters in your password?");
 
   return [passwordLength, lower, upper, numbers, special]
 }
@@ -116,7 +120,30 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
+  let arr = [];
+  let password = "";
   [passwordLength, lower, upper, numbers, special] = getPasswordOptions();
+  if (!( lower || upper || numbers || special )) {
+    return "No valid Password option chosen\n please try again"
+  }
+  if (lower) {
+    arr = arr.concat(lowerCasedCharacters);
+  }
+  if (lower) {
+    arr = arr.concat(upperCasedCharacters);
+  }
+  if (lower) {
+    arr = arr.concat(numericCharacters);
+  }
+  if (lower) {
+    arr = arr.concat(specialCharacters);
+  }
+  // password length to get characters
+  for (let i = 0; i < passwordLength; i++) {
+    password += getRandom(arr); 
+  }
+
+  return password;
   return [passwordLength, lower, upper, numbers, special];
 }
 
